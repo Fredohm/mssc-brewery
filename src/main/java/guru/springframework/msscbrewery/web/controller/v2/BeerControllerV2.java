51d1a026349f58bd0830c9de65a3083391e67171
Fredohm <fredwaucquez@hotmail.fr>
2021-03-24 07:02:48 +0100

    75. Lombok examples

--- src/main/java/guru/springframework/msscbrewery/web/controller/v2/BeerControllerV2.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/v2/BeerControllerV2.java
@@ -4,19 +4,19 @@ package guru.springframework.msscbrewery.web.controller.v2;
 import guru.springframework.msscbrewery.services.v2.BeerServiceV2;
 import guru.springframework.msscbrewery.web.model.v2.BeerDtoV2;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
+@Slf4j
 @Validated
 @RequiredArgsConstructor
 @RequestMapping("/api/v2/beer")
@@ -34,9 +34,11 @@ public class BeerControllerV2 {
     @PostMapping
     public ResponseEntity<BeerDtoV2> handlePost(@Valid @NotNull @RequestBody BeerDtoV2 beerDto) {
 
-        BeerDtoV2 savedDto = beerServiceV2.saveNewBeer(beerDto);
+        log.debug("Handle post...");
 
-        HttpHeaders headers = new HttpHeaders();
+        val savedDto = beerServiceV2.saveNewBeer(beerDto);
+
+        val headers = new HttpHeaders();
         // todo add hostname to url
         headers.add("Location", "/api/v1/beer/" + savedDto.getId().toString());
 
